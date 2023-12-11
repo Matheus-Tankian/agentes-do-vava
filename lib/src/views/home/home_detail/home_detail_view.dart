0de04ba29a2711aa1d49a11bdbd2d5ce3317d4c1
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2023-12-11 14:53:10 -0300

    ajustando detalhes

--- lib/src/views/home/home_detail/home_detail_view.dart
+++ lib/src/views/home/home_detail/home_detail_view.dart
@@ -14,64 +14,78 @@ class HomeDetailView extends StatefulWidget {
 class _HomeDetailViewState extends State<HomeDetailView> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            height: 230,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: AppColors.viperColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.viperColor,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 190,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: AppColors.viperColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              child: Image.asset(
+                AppImages.viper,
+                fit: BoxFit.contain,
               ),
             ),
-            child: Image.asset(
-              AppImages.viper,
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 35),
-            child: Column(
-              children: [
-                const SectionText(
-                  title: 'BIOGRAPHY',
-                ),
-                Text(
-                  'A phantom of a memory. Omen hunts in the shadows. He renders enemies blind, teleports across the field.',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(fontSize: 15, color: AppColors.colorWhite),
-                ),
-                const SizedBox(height: 20),
-                const SectionText(
-                  title: 'SPECEIAL ABILITIES',
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
+            const SizedBox(height: 20),
+            Padding(
               padding: const EdgeInsets.symmetric(horizontal: 35),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return const Padding(
-                  padding: EdgeInsets.only(bottom: 26),
-                  child: PowerCard(
-                    imagem: AppImages.power,
-                    title: 'PARANOIA',
-                    subtitle: 'Shadow power',
+              child: Column(
+                children: [
+                  const SectionText(
+                    title: 'BIOGRAPHY',
                   ),
-                );
-              },
+                  Text(
+                    'A phantom of a memory. Omen hunts in the shadows. He renders enemies blind, teleports across the field.',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium!
+                        .copyWith(fontSize: 15, color: AppColors.colorWhite),
+                  ),
+                  const SizedBox(height: 20),
+                  const SectionText(
+                    title: 'SPECEIAL ABILITIES',
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 35),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(bottom: 26),
+                    child: PowerCard(
+                      imagem: AppImages.power,
+                      title: 'PARANOIA',
+                      subtitle: 'Shadow power',
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
