d4426dbedc7156831f866a02d5b82b5df8bf44a7
Matheus Vitor de Oliveira Cruz <61215783+Matheus-Tankian@users.noreply.github.com>
2023-12-12 10:01:38 -0300

    configurando banco

--- lib/src/views/home/home_viewmodel.dart
+++ lib/src/views/home/home_viewmodel.dart
@@ -1,11 +1,50 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:vava/src/services/http/exeptions.dart';
+import 'package:vava/src/views/home/repositories/home_repositories.dart';
 
 class HomeViewModel extends ChangeNotifier {
+  final ImpleAgentRepository repository;
+
+  bool _disposed = false;
+
   int _indexScreen = 0;
   int get indexScreen => _indexScreen;
 
   changeIndexScreen(int value) {
     _indexScreen = value;
     notifyListeners();
+  }
+
+  HomeViewModel({required this.repository}) {
+    getProdutos();
+  }
+
+  Future<void> getProdutos() async {
+    log('aq');
+    try {
+      final result = await repository.getAgents();
+      log('${result.first.agentName}');
+    } on NotFoundExeption catch (e) {
+      e.message;
+      log(e.message);
+    } catch (e) {
+      e.toString();
+      log(e.toString());
+    }
+  }
+
+  @override
+  void dispose() {
+    _disposed = true;
+    super.dispose();
+  }
+
+  @override
+  void notifyListeners() {
+    if (!_disposed) {
+      super.notifyListeners();
+    }
   }
 }
--- lib/src/views/home/repositories/repositories.dart
+++
@@ -1,4 +0,0 @@
-import 'package:vava/src/services/http/http_api.dart';
-
-final ImpleHttpClient client;
-
